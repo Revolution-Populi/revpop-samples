baf07fdab5ea1c040734f64a752494ef10e5e106
GoodWien <mailto:89968171+GoodWien@users.noreply.github.com>
2021-09-29 15:59:11 +0300

    Update case0-bootstrap and sample5-committee

--- case0-bootstrap.js
+++ case0-bootstrap.js
@@ -20,47 +20,146 @@ require('dotenv').config();
 const { PrivateKey } = require('@revolutionpopuli/revpopjs');
 const revpop = require('./lib/revpop');
 
+const common_accounts = [
+    { nick: 'registrar', name: 'nathan' },
+];
+
+const committee_member_accounts = [
+    { name: 'init0' },
+    { name: 'init1' },
+    { name: 'init2' },
+    { name: 'init3' },
+    { name: 'init4' },
+    { name: 'init5' },
+    { name: 'init6' },
+    { name: 'init7' },
+    { name: 'init8' },
+    { name: 'init9' },
+    { name: 'init10' },
+];
+
+async function connect_to_network() {
+    const connect_string = process.env.BLOCKCHAIN_URL;
+    console.log(`Connecting to ${connect_string}`);
+    const network = await revpop.connect(connect_string);
+    console.log(`Connected to network ${network.network_name}`);
+    console.log(``);
+}
+
+async function close_network_connection() {
+    console.log(`Disconnect from RevPop`);
+    await revpop.disconnect();
+}
+
+async function init_accounts(accounts) {
+    for (const account of accounts) {
+        account.acc = await revpop.db_exec('get_account_by_name', account.name);
+        account.key = PrivateKey.fromWif('5KXbCDyCPL3eGX6xX5uJHVwoAYheF7L5fKf67oQocgJA8kNvVHF');
+        console.log(`Account ${account.nick ? account.nick + ' ' : ''}${account.name}: ${account.acc ? account.acc.id : '[no account yet]'}`);
+    }
+}
+
+async function init_registrar(registrar) {
+    console.log(`Getting balance ${revpop.config.balance_address}...`);
+    const init_balance = await revpop.query_balance(revpop.config.balance_address);
+    if (init_balance) {
+        console.log(`Balance ${init_balance.owner} ${init_balance.id} ` +
+            `${init_balance.balance.amount} ${init_balance.balance.asset_id}`);
+        console.log(``);
+
+        console.log(`Account ${registrar.acc.name} claim balance...`);
+        await revpop.claim_balance(registrar, init_balance);
+        console.log(`Balance of account ${registrar.acc.name} claimed`);
+        console.log(``);
+
+        console.log(`Upgrading registrar account ${registrar.acc.name}...`);
+        await revpop.upgrade_account(registrar);
+        console.log(`Registrar account ${registrar.acc.name} upgraded`);
+        console.log(``);
+
+    } else {
+        console.log(`Balance ${revpop.config.balance_address} already claimed`);
+        console.log(``);
+    }
+}
+
+async function get_committee_member(committee_member_acc_name) {
+    const committee_member = revpop.db_exec('get_committee_member_by_account', committee_member_acc_name);
+    if (!committee_member) {
+        throw new Error(`No committee_member with account ${committee_member_acc_name} found!`);
+    }
+    return committee_member;
+}
+
+async function get_committee_members(committee_member_names) {
+    const committee_members_promises = [];
+    for (const committee_member of committee_member_names) {
+        committee_members_promises.push(get_committee_member(committee_member.name));
+    }
+    return Promise.all(committee_members_promises);
+}
+
+async function vote_for_committee_members(voter, committee_members) {
+    if (committee_members[0].total_votes > 0) {
+        console.log(`Committee members already voted`);
+        return;
+    }
+
+    const new_options = JSON.parse(JSON.stringify(voter.acc.options));
+    new_options.votes = committee_members.map(committee_member => committee_member.vote_id);
+    new_options.votes.sort();
+    new_options.num_committee = new_options.votes.length;
+    new_options.num_witness = 0;
+    await revpop.transaction(voter.key, 'account_update', {
+        fee: revpop.no_fee(),
+        account: voter.acc.id,
+        new_options: new_options,
+    });
+}
+
+async function transfer_to_accounts(issuer, accounts, amount) {
+    const acc_balance = await revpop.db_exec('get_account_balances', accounts[0].name, []);
+    if (acc_balance[0] !== undefined && acc_balance[0].amount > 0) {
+        console.log(`Committee members balances already updated`);
+        return;
+    }
+
+    let transfers = [];
+    for (const account of accounts) {
+        console.log(`Transfer ${amount} RVP to account ${account.name}`);
+        transfers.push(revpop.transfer(issuer, account, amount));
+    };
+    await Promise.all(transfers);
+}
+
+async function prepare_registrar_and_committee() {
+    console.log(`Initialize all accounts...`);
+    await init_accounts(common_accounts);
+    await init_accounts(committee_member_accounts);
+
+    console.log(``);
+    console.log(`Prepare registrar account...`);
+    const [registrar] = common_accounts;
+    await init_registrar(registrar);
+
+    const committee_members = await get_committee_members(committee_member_accounts);
+
+    console.log(`Voting for committee members ${JSON.stringify(committee_members.map(committee_member => committee_member.id))}...`);
+    await vote_for_committee_members(registrar, committee_members);
+    console.log(`OK`);
+    console.log(``);
+
+    console.log(`Update committee members balances to let them pay fee for operations...`);
+    await transfer_to_accounts(registrar, committee_member_accounts, 10000 * 10 ** 5);
+    console.log(`OK`);
+    console.log(``);
+}
+
 async function case0_bootstrap() {
     try {
-        const connect_string = process.env.BLOCKCHAIN_URL;
-        console.log(`Connecting to ${connect_string}...`);
-        const network = await revpop.connect(connect_string);
-        console.log(`Connected to network ${network.network_name}`);
-        console.log(``);
+        await connect_to_network();
 
-        console.log(`Initialize all accounts...`);
-        const accounts = [
-            { nick: 'registrar', name: 'nathan' },
-        ];
-        const [ registrar ] = accounts;
-        for (const account of accounts) {
-            account.acc = await revpop.db_exec('get_account_by_name', account.name);
-            account.key = PrivateKey.fromWif('5KXbCDyCPL3eGX6xX5uJHVwoAYheF7L5fKf67oQocgJA8kNvVHF');
-            console.log(`Account ${account.nick} ${account.name} ${account.acc ? account.acc.id : '[no account yet]'}`);
-        }
-        console.log(``);
-
-        console.log(`Getting balance ${revpop.config.balance_address}...`);
-        const init_balance = await revpop.query_balance(revpop.config.balance_address);
-        if (init_balance) {
-            console.log(`Balance ${init_balance.owner} ${init_balance.id} ` +
-                        `${init_balance.balance.amount} ${init_balance.balance.asset_id}`);
-            console.log(``);
-
-            console.log(`Account ${registrar.acc.name} claim balance...`);
-            await revpop.claim_balance(registrar, init_balance);
-            console.log(`Balance of account ${registrar.acc.name} claimed`);
-            console.log(``);
-
-            console.log(`Upgrading registrar account ${registrar.acc.name}...`);
-            await revpop.upgrade_account(registrar);
-            console.log(`Registrar account ${registrar.acc.name} upgraded`);
-            console.log(``);
-
-        } else {
-            console.log(`Balance ${revpop.config.balance_address} already claimed`);
-            console.log(``);
-        }
+        await prepare_registrar_and_committee();
 
     } catch (err) {
         console.error(`Error:`, err);
@@ -68,14 +167,18 @@ async function case0_bootstrap() {
             console.error(err.data.stack);
         console.log(``);
     }
-
-    console.log(`Close connection`);
-    await revpop.disconnect();
-    console.log(``);
 }
 
-exports.case0_bootstrap = case0_bootstrap;
+module.exports = {
+    common_accounts,
+    committee_member_accounts,
+    case0_bootstrap,
+    connect_to_network,
+    close_network_connection,
+    prepare_registrar_and_committee
+}
 
 if (require.main === module) {
-    case0_bootstrap();
+    const { run_func } = require('./index');
+    run_func(case0_bootstrap, close_network_connection);
 }
--- sample-5-committee.js
+++ sample-5-committee.js
@@ -21,32 +21,35 @@ const assert = require('assert');
 const path = require('path');
 const { PrivateKey, TransactionBuilder } = require('@revolutionpopuli/revpopjs');
 const revpop = require('./lib/revpop');
+const bootstrap = require('./case0-bootstrap.js');
 
 const pkey = PrivateKey.fromWif('5KXbCDyCPL3eGX6xX5uJHVwoAYheF7L5fKf67oQocgJA8kNvVHF');
 
-const accounts = [
-    { name: 'init0' },
-    { name: 'init1' },
-    { name: 'init2' },
-    { name: 'init3' },
-    { name: 'init4' },
-    { name: 'init5' },
-    { name: 'init6' },
-    { name: 'init7' },
-    { name: 'init8' },
-    { name: 'init9' },
-    { name: 'init10' },
-];
+async function wait_until(stop_time_s) {
+    const now_ms = Date.now();
+    const stop_time_ms = stop_time_s * 1000;
+    if (now_ms > stop_time_ms)
+        return;
 
-function cup_op(parameters) {
+    const wait_s = Math.round((stop_time_ms - now_ms)/1000);
+    console.log(`Waiting ${wait_s} seconds...`);
+
+    while (Date.now() < stop_time_ms) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        process.stdout.write(`.`);
+    }
+    console.log(``);
+}
+
+async function wait(ms) {
+    await wait_until((Date.now() + ms)/1000);
+}
+
+function committee_member_rp_vm_update_operation(parameters, new_rp_vm) {
     const new_parameters = Object.assign(
         parameters,
         {
-            current_fees: {
-                parameters: [],
-                scale: 10000
-            },
-            revpop_vote_mixture: 6
+            revpop_vote_mixture: new_rp_vm
         }
     );
 
@@ -56,52 +59,33 @@ function cup_op(parameters) {
     };
 }
 
-async function sample_5_committee() {
-    // Connect to blockchain
-    const connect_string = process.env.BLOCKCHAIN_URL;
-    console.log(`Connecting to ${connect_string}...`);
-    const network = await revpop.connect(connect_string);
-    console.log(`Connected to network: ${network.network_name}`);
-    console.log(``);
-
-    for (const account of accounts) {
-        account.account = await revpop.db_exec('get_account_by_name', account.name);
-        account.key = pkey;
-    }
-
-    const make_proposal_result = await make_proposal(accounts[0]);
-    console.log('make_proposal', make_proposal_result);
-
-    const proposal_id = make_proposal_result;
-    const vote_proposal_result = await Promise.all(accounts.map((account) => {
-        return vote_for_proposal(account, proposal_id)
-    }));
-    console.log('vote_for_proposal', vote_proposal_result);
-}
-
-async function make_proposal(member) {
+async function committee_member_make_proposal_update_revpop_vote_mixture(member, new_rp_vm) {
     const pkey = member.key;
     const global_properties = await revpop.db_exec('get_global_properties');
     const parameters = global_properties.parameters;
     const review_period = parameters.committee_proposal_review_period;
     // 120 - 2 min for voting
-    const expiration_time = Math.ceil(Date.now() / 1000) + review_period + 120;
+    const expiration_time = Math.ceil(Date.now() / 1000) + review_period + 20;
 
     const txb = new TransactionBuilder();
     await txb.update_head_block();
-    txb.add_type_operation('committee_member_update_global_parameters', cup_op(parameters));
+    txb.add_type_operation('committee_member_update_global_parameters', committee_member_rp_vm_update_operation(parameters, new_rp_vm));
     await txb.set_required_fees();
     txb.propose({
         fee: revpop.fee(1206522),
-        fee_paying_account: member.account.id,
+        fee_paying_account: member.acc.id,
         expiration_time: expiration_time,
         review_period_seconds: review_period
     });
     txb.add_signer(pkey, pkey.toPublicKey().toPublicKeyString());
     const result = await txb.broadcast();
-    const operation_results = result[0].trx.operation_results[0];
 
-    return operation_results[1];
+    const operation_results = {};
+    operation_results.id = result[0].trx.operation_results[0][1];
+    const prop_obj = await revpop.db_exec('get_objects', [operation_results.id]);
+    operation_results.expiration_time = prop_obj[0].expiration_time;
+    operation_results.expiration = expiration_time;
+    return operation_results;
 }
 
 async function vote_for_proposal(member, proposal_id) {
@@ -109,10 +93,10 @@ async function vote_for_proposal(member, proposal_id) {
     const txb = new TransactionBuilder();
     await txb.update_head_block();
     txb.add_type_operation('proposal_update', {
-        fee_paying_account: member.account.id,
+        fee_paying_account: member.acc.id,
         proposal: proposal_id,
         active_approvals_to_add: [
-            member.account.id
+            member.acc.id
         ]
     });
     await txb.set_required_fees();
@@ -123,12 +107,45 @@ async function vote_for_proposal(member, proposal_id) {
     return operation_results[1];
 }
 
-async function finalizer() {
-    console.log(`Disconnect from RevPop...`);
-    await revpop.disconnect();
+async function sample_5_committee() {
+    await bootstrap.connect_to_network();
+    await bootstrap.prepare_registrar_and_committee();
+
+    for (const account of bootstrap.committee_member_accounts) {
+        account.key = pkey;
+    }
+
+    const gp = await revpop.db_exec('get_global_properties');
+    const rp_vm = gp.parameters.revpop_vote_mixture;
+    const new_rp_vm = rp_vm + 1;
+    console.log(`We going to change revpop_vote_mixture parameter of global properties from ${rp_vm} to ${new_rp_vm}`);
+
+    const make_proposal_result = await committee_member_make_proposal_update_revpop_vote_mixture(bootstrap.committee_member_accounts[0], new_rp_vm);
+    console.log(`make_proposal: ${make_proposal_result.id}, expiration: ${make_proposal_result.expiration_time}`);
+
+    const proposal_id = make_proposal_result.id;
+    const vote_proposal_result = await Promise.all(bootstrap.committee_member_accounts.map((acc) => {
+        return vote_for_proposal(acc, proposal_id)
+    }));
+    console.log('vote_for_proposal', vote_proposal_result);
+
+    console.log(`Waiting for proposal to implement at ${make_proposal_result.expiration_time}`);
+    await wait_until(make_proposal_result.expiration);
+
+    const dgp = await revpop.db_exec('get_dynamic_global_properties');
+    const block_time = Date.parse(dgp.time);
+    const next_maintenance_time = Date.parse(dgp.next_maintenance_time);
+    const wait_for_maintenance_ms = next_maintenance_time - block_time;
+    console.log(``);
+
+    console.log(`Waiting for the next maintenance interval at ${dgp.next_maintenance_time}`);
+    await wait(wait_for_maintenance_ms + 1000);
+
+    const gp_new = await revpop.db_exec('get_global_properties');
+    console.log(`Currently revpop_vote_mixture parameter of global properties is ${gp_new.parameters.revpop_vote_mixture}`);
 }
 
 if (require.main === module) {
     const { run_func } = require('./index');
-    run_func(sample_5_committee, finalizer);
+    run_func(sample_5_committee, bootstrap.close_network_connection);
 }
